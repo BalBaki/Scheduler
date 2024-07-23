b65f5a53aa6d1cb1f4c6126a5d4b52e9a23ade3e
Baki <bakibal1994@gmail.com>
2024-07-23 13:19:03 +0300

    Added crop functionality to profile picture change

---
+++ src/components/profile/account/image-crop.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import 'react-image-crop/dist/ReactCrop.css';
+import { type Dispatch, type SetStateAction, useEffect, useRef, useState } from 'react';
+import ReactCrop, { type Crop, type PixelCrop, centerCrop, makeAspectCrop } from 'react-image-crop';
+import Image from 'next/image';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
+
+type ImageCropProps = {
+    imageUrl: string;
+    setCroppedImage: Dispatch<SetStateAction<File | null>>;
+    disabled?: boolean;
+};
+
+export default function ImageCrop({ imageUrl, setCroppedImage, disabled }: ImageCropProps) {
+    const [crop, setCrop] = useState<Crop>();
+    const [isShowDialog, setIsShowDialog] = useState(!!imageUrl);
+    const [completedCrop, setCompletedCrop] = useState<PixelCrop>();
+    const imageRef = useRef<HTMLImageElement | null>(null);
+
+    useEffect(() => {
+        imageUrl && setIsShowDialog(true);
+    }, [imageUrl]);
+
+    const centerAspectCrop = (mediaWidth: number, mediaHeight: number, aspect: number) =>
+        centerCrop(
+            makeAspectCrop(
+                {
+                    unit: '%',
+                    width: 90,
+                },
+                aspect,
+                mediaWidth,
+                mediaHeight
+            ),
+            mediaWidth,
+            mediaHeight
+        );
+
+    const handleCropClick = async () => {
+        if (!imageRef.current || !completedCrop) return;
+
+        const { naturalWidth, naturalHeight, width, height } = imageRef.current;
+        const scaleX = naturalWidth / width;
+        const scaleY = naturalHeight / height;
+        const offscreen = new OffscreenCanvas(completedCrop.width * scaleX, completedCrop.height * scaleY);
+        const ctx = offscreen.getContext('2d');
+
+        if (!ctx) return;
+
+        ctx.translate(-completedCrop.x * scaleX, -completedCrop.y * scaleY);
+        ctx.drawImage(imageRef.current, 0, 0, naturalWidth, naturalHeight, 0, 0, naturalWidth, naturalHeight);
+
+        const blob = await offscreen.convertToBlob({
+            type: 'image/png',
+        });
+        const file = new File([blob], 'croppedImage.png', {
+            type: 'image/png',
+        });
+
+        file && setCroppedImage(file);
+        setIsShowDialog(false);
+    };
+    const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        const { width, height } = event.currentTarget;
+
+        setCrop(centerAspectCrop(width, height, 1));
+    };
+
+    return (
+        <Dialog open={isShowDialog} onOpenChange={setIsShowDialog}>
+            <DialogTrigger asChild>
+                <Button className="text-center" disabled={disabled}>
+                    Crop
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Crop Image</DialogTitle>
+                    <DialogDescription asChild className="text-center">
+                        <div>
+                            <VisuallyHidden.Root>Crop Image</VisuallyHidden.Root>
+                            <ReactCrop
+                                crop={crop}
+                                onChange={(c) => setCrop(c)}
+                                circularCrop={true}
+                                locked={true}
+                                onComplete={(c) => setCompletedCrop(c)}
+                            >
+                                <div className="relative flex justify-center">
+                                    <Image
+                                        ref={imageRef}
+                                        src={imageUrl}
+                                        alt="Crop Picture"
+                                        className="object-contain w-60 h-auto"
+                                        width="0"
+                                        height="0"
+                                        priority={true}
+                                        onLoad={handleImageLoad}
+                                    />
+                                </div>
+                            </ReactCrop>
+                            <div>
+                                <Button onClick={handleCropClick}>Crop</Button>
+                            </div>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+}
--- src/components/profile/account/profile-picture.tsx
+++ src/components/profile/account/profile-picture.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Image from 'next/image';
 import { CgProfile } from 'react-icons/cg';
 import { toast } from 'react-toastify';
@@ -9,15 +9,23 @@ import { useSession } from 'next-auth/react';
 import { ImSpinner6 } from 'react-icons/im';
 import { useMutation } from '@tanstack/react-query';
 import { Button } from '../../ui/button';
+import ImageCrop from './image-crop';
 
 export default function ProfilePicture() {
     const { data: session, update: updateSession } = useSession();
-    const [picture, setPicture] = useState<File | null>(null);
+    const [image, setImage] = useState<File | null>(null);
+    const [croppedImage, setCroppedImage] = useState<File | null>(null);
+    const imageUrl = useMemo(() => {
+        return image ? URL.createObjectURL(image as Blob) : '';
+    }, [image]);
+
     const { mutate: uploadPicture, isPending } = useMutation({
         mutationFn: updateProfilePicture,
         onSuccess({ update, error }) {
             if (update) {
-                updateSession().then(() => setPicture(null));
+                updateSession().then(() => {
+                    setImage(null);
+                });
             }
 
             toast(update ? 'Successfully Changed' : error, {
@@ -25,25 +33,35 @@ export default function ProfilePicture() {
             });
         },
     });
-
     const handlePictureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (!event.target.files || event.target.files.length < 1) return;
 
-        setPicture(event.target.files[0]);
+        setImage(event.target.files[0]);
+    };
+    const handleCancelClick = () => {
+        setImage(null);
     };
 
+    useEffect(() => {
+        setCroppedImage(null);
+    }, [image]);
+
     return (
-        <div className="max-w-96 h-56">
-            <form action={uploadPicture} className="flex flex-col items-center justify-center">
+        <div className="max-w-96 flex flex-col justify-center gap-y-1">
+            <div className="flex flex-col items-center justify-center">
                 <label htmlFor={isPending ? '' : 'profilePicture'}>
-                    <div className="relative size-40 overflow-hidden">
-                        {picture || session?.user.imageUrl ? (
+                    <div className="relative size-[200px]  overflow-hidden">
+                        {image || session?.user.imageUrl ? (
                             <Image
-                                src={picture ? URL.createObjectURL(picture as Blob) : session?.user.imageUrl || ''}
+                                src={
+                                    image || croppedImage
+                                        ? URL.createObjectURL((croppedImage || image) as Blob)
+                                        : session?.user.imageUrl || ''
+                                }
                                 alt="profile picture"
                                 fill
-                                className="object-cover rounded-full"
                                 sizes="10rem, 10rem"
+                                className="rounded-full aspect-square"
                                 priority={true}
                             />
                         ) : (
@@ -59,12 +77,30 @@ export default function ProfilePicture() {
                     onChange={handlePictureChange}
                     className="hidden"
                 />
-                {picture && (
-                    <Button disabled={isPending} className="mt-2">
-                        {isPending ? <ImSpinner6 className="w-full h-full animate-spin" /> : 'Save'}
-                    </Button>
+                {image && (
+                    <div className="flex items-center gap-x-1 mt-2">
+                        <ImageCrop imageUrl={imageUrl} setCroppedImage={setCroppedImage} disabled={isPending} />
+                        <Button
+                            disabled={isPending}
+                            className="w-16"
+                            onClick={() => {
+                                if (!image) return;
+
+                                const formData = new FormData();
+
+                                formData.append('profilePicture', croppedImage || image);
+
+                                uploadPicture(formData);
+                            }}
+                        >
+                            {isPending ? <ImSpinner6 className="w-full h-full animate-spin" /> : 'Save'}
+                        </Button>
+                        <Button onClick={handleCancelClick} disabled={isPending}>
+                            Cancel
+                        </Button>
+                    </div>
                 )}
-            </form>
+            </div>
         </div>
     );
 }

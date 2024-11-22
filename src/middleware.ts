7d8e4b41846092bd83980af3fe19fb86ba657bb1
Baki <bakibal1994@gmail.com>
2024-11-22 16:44:30 +0300

    changed mw logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,6 @@
-import { NextResponse } from 'next/server';
 import { auth } from './auth';
-import { hasPermission } from './lib/permissions';
 
-export default auth((request) => {
-    const { pathname } = request.nextUrl;
-
-    if (
-        request.auth?.user &&
-        (pathname.startsWith('/login') || pathname.startsWith('/register'))
-    ) {
-        return NextResponse.redirect(new URL('/', request.url));
-    }
-
-    if (
-        (!request.auth?.user && pathname.startsWith('/profile')) ||
-        (pathname.startsWith('/dashboard') &&
-            (!request.auth ||
-                !hasPermission(request.auth.user, 'dashboard', 'view')))
-    ) {
-        return NextResponse.redirect(new URL('/login', request.url));
-    }
-});
+export default auth;
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],

251ff6e4b35fbeaafd831d2e364e026b2cc48f98
Baki <bakibal1994@gmail.com>
2024-11-26 11:16:44 +0300

    Fixed build error at NProgressbar

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import NProgressBarProvider from '@/providers/NProgressBar';
@@ -14,13 +15,14 @@ export default function Providers({ children }: ProvidersProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <SessionProvider>
-                <NProgressBarProvider
-                    showSpinner={false}
-                    easing="ease"
-                    speed={300}
-                >
-                    {children}
-                </NProgressBarProvider>
+                {children}
+                <Suspense>
+                    <NProgressBarProvider
+                        showSpinner={false}
+                        easing="ease"
+                        speed={300}
+                    />
+                </Suspense>
             </SessionProvider>
         </QueryClientProvider>
     );
